8764afbc33ddbaab7ff3b9f5a5839e8f2e10ab94
JoaoBagatinPivetaAssuncao <joaobpivetaa@gmail.com>
2026-04-18 17:02:41 -0300

    Create notion.js

---
+++ netlify/functions/notion.js
@@ -0,0 +1,33 @@
+javascriptexports.handler = async (event) => {
+  const token = process.env.NOTION_TOKEN;
+  const path = event.path.replace('/.netlify/functions/notion', '').replace('/api/notion', '');
+  const notionUrl = `https://api.notion.com/v1${path}`;
+
+  const headers = {
+    'Authorization': `Bearer ${token}`,
+    'Notion-Version': '2022-06-28',
+    'Content-Type': 'application/json',
+  };
+
+  try {
+    const response = await fetch(notionUrl, {
+      method: event.httpMethod === 'GET' ? 'GET' : event.httpMethod,
+      headers,
+      body: event.body || undefined,
+    });
+    const data = await response.json();
+    return {
+      statusCode: response.status,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify(data),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
